603d41faf9025f4b7a4947c569fd443b67b85766
Wyatt-T <w.townsendd@gmail.com>
2020-03-08 22:44:36 +0000

    Front end done

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,12 +1,70 @@
-import React, { Component } from 'react';
-import { Form, Row, Col } from "react-bootstrap";
+import React, { Component, useState } from 'react';
+import { useDispatch } from 'react-redux'
+import { Form, Col, Modal, Button, Drop} from "react-bootstrap";
+import { login } from "../../redux/user.redux";
+
 
 const Login = (props) => {
+
+   
+    const dispacher = useDispatch();
+  
+    const [credentialsForm, setCredentialsForm] = useState({
+        username: "",
+        password: "",
+      });
+    
+
+   const formUpdate = e => {      
+       setCredentialsForm({[e.target.name]: e.target.value});      
+    };
+
+   const handleLogin = e => {
+     
+        dispacher(login(credentialsForm));
+
+    }; 
+
+     
     return (
-        <div>
-            login
-        </div>
+        <Modal.Dialog>
+        <Modal.Header>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+      
+        <Modal.Body>
+           <Form>
+
+               <Form.Group as={Col}>
+                   <Form.Label>Username</Form.Label>
+                   <Form.Control
+                       type="text"
+                       value={credentialsForm.username}
+                       onChange={formUpdate}
+                   ></Form.Control> 
+               </Form.Group> {/* End of username form*/}
+
+               <Form.Group as={Col}>
+                   <Form.Label>Password</Form.Label>
+                   <Form.Control
+                       type="password"
+                       value={credentialsForm.password}
+                       onChange={formUpdate}
+                   ></Form.Control>  
+               </Form.Group>  {/* End of password form*/}
+
+           </Form>
+        </Modal.Body>
+      
+        <Modal.Footer>
+          <Button variant="outline-primary" onClick={handleLogin}>Submit</Button>         
+        </Modal.Footer>
+
+      </Modal.Dialog>
     )
 }
 
 export default Login
+
+/* Source https://react-bootstrap.github.io/components/modal/ */
+
