01fa934487526b76cb700fae68397fb27ebb9096
LutongZhang <aa1264126181@gmail.com>
2020-03-09 20:23:30 -0400

    userCenter

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 const db = {
   uri:
-    "mongodb+srv://mwollenhaup:CENPASSWORD@cluster0-8be66.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://Lutong:1qsz2waxW!@cluster0-b4o6r.mongodb.net/test?retryWrites=true&w=majority"
 };
 //node -e "console.log(require('crypto').randomBytes(32).toString('hex'));"
 const config = {
