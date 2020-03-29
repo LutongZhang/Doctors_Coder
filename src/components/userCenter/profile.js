11f0ac783a2fb992f8038b856ddf26e027361f8c
jeremytriplett <jeremytriplett@ufl.edu>
2020-03-29 11:15:03 +0000

    Update profile.js

--- src/components/userCenter/profile.js
+++ src/components/userCenter/profile.js
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const Profile = props => {
-  console.log("Profile");
-  return <div>profile</div>;
+	const [checkedOut, setCheckedOut] = useState([]);
+	const user = useSelector(state => state.user);
+
+	const getUserCheckout = () => {
+		 axios
+      .post("/api/devices/getUserCheckout",{userName: user.userName})
+      .then(res => {
+		  setCheckedOut(res.data);
+      })
+      .catch(err => {
+      });	
+	}
+	getUserCheckout();
+  const list = checkedOut.map(checkedOut=>{
+	  
+	  return(
+	  <tr>
+  <td> <b>{checkedOut.userName} </b></td>
+  <td> <i>{checkedOut.device}</i> </td>
+  <td> {checkedOut.checkoutTime} </td>
+  </tr>);
+  });
+  
+  return <div><h1>Checkout history for {user.userName}</h1>{list}</div>;
+ 
 };
 
 export default Profile;
