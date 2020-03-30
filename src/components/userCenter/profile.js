18dd554fe4897edbd3ecdf04f4860dc878a2ddff
LutongZhang <aa1264126181@gmail.com>
2020-03-29 20:32:44 -0400

    bug fixed

--- src/components/userCenter/profile.js
+++ src/components/userCenter/profile.js
@@ -1,33 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 
 const Profile = props => {
-	const [checkedOut, setCheckedOut] = useState([]);
-	const user = useSelector(state => state.user);
+  const [checkedOut, setCheckedOut] = useState([]);
+  const user = useSelector(state => state.user);
 
-	const getUserCheckout = () => {
-		 axios
-      .post("/api/devices/getUserCheckout",{userName: user.userName})
+  const getUserCheckout = () => {
+    axios
+      .post("/api/devices/getUserCheckout", { userName: user.userName })
       .then(res => {
-		  setCheckedOut(res.data);
+        setCheckedOut(res.data);
       })
-      .catch(err => {
-      });	
-	}
-	getUserCheckout();
-  const list = checkedOut.map(checkedOut=>{
-	  
-	  return(
-	  <tr>
-  <td> <b>{checkedOut.userName} </b></td>
-  <td> <i>{checkedOut.device}</i> </td>
-  <td> {checkedOut.checkoutTime} </td>
-  </tr>);
+      .catch(err => {});
+  };
+  useEffect(() => {
+    getUserCheckout();
+  }, []);
+  const list = checkedOut.map(checkedOut => {
+    return (
+      <tr>
+        <td>
+          {" "}
+          <b>{checkedOut.userName} </b>
+        </td>
+        <td>
+          {" "}
+          <i>{checkedOut.device}</i>{" "}
+        </td>
+        <td> {checkedOut.checkoutTime} </td>
+      </tr>
+    );
   });
-  
-  return <div><h1>Checkout history for {user.userName}</h1>{list}</div>;
- 
+
+  return (
+    <div>
+      <h1>Checkout history for {user.userName}</h1>
+      <table>{list}</table>
+    </div>
+  );
 };
 
 export default Profile;
